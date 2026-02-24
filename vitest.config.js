a98d540aa5f2a3eff9cf7da88c80e22a3b180ba9
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-24 09:43:59 +0100

    Add jsdom

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,11 @@
-// import config from "./vite.config";
-// import { defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
-// export default defineConfig({
-//   plugins: [...config.plugins],
-//   test: {
-//     // This means we don't have to import `describe`,
-//     // `it`, and `expect` into every test file
-//     globals: true,
-//   },
-// });
+export default defineConfig({
+  test: {
+    environmentOptions: {
+      jsdom: {
+        url: "http://localhost:3000",
+      },
+    },
+  },
+});
